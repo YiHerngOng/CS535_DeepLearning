97a594884d90f2f0a2e9206d3d4bdc0f2457326f
YiHerngOng <ongyi@oregonstate.edu>
2019-02-05 11:23:21 -0800

    first commit

---
+++ hw2/hw2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+CS 535 Deep Learning Assignment 2
+Author: Yi Herng Ong
+Description: TBA
+'''
+
+
+import os, sys
+import numpy as np
+
+
+def evaluation_(nn):
+	pass
+
+def backpropagation():
+	pass
+
+
+
+if __name__ == '__main__':
+	pass
